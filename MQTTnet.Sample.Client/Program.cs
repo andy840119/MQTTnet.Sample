f09f4bde93714fadc30d60163bce697d082968cf
為什麼 <andy840119@gmail.com>
2019-03-15 16:19:38 +0800

    add publisher / server(and receiver) project for testing

--- MQTTnet.Sample.Client/Program.cs
+++ MQTTnet.Sample.Client/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using MQTTnet.Client;
+using MQTTnet.Protocol;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MQTTnet.Sample.Client
 {
     class Program
     {
-        static void Main(string[] args)
+        //Note : change IP if needed
+        protected static string ServerAddress => "localhost";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Sending properties
+            var topic = "andy840119/iot";
+            var quality = MqttQualityOfServiceLevel.AtLeastOnce;
+
+            //Run a MQTT publish client
+            var publishClient = new MqttFactory().CreateMqttClient();
+            await publishClient.ConnectAsync(new MqttClientOptionsBuilder().WithTcpServer(ServerAddress).Build());
+
+            while(true)
+            { 
+                Console.WriteLine("Type any message to send message from publisher to subscripter...");
+                string line = Console.ReadLine();
+
+                Console.WriteLine("Message sent : " + line);
+                await publishClient.PublishAsync(new MqttApplicationMessage()
+                { 
+                    Topic = topic,
+                    QualityOfServiceLevel = quality,
+                    Payload = Encoding.UTF8.GetBytes(line),
+                });
+
+                await Task.Delay(500);
+            }
         }
     }
 }
--- MQTTnet.Sample.Server/Program.cs
+++ MQTTnet.Sample.Server/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using MQTTnet.Client;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MQTTnet.Sample.Server
 {
     class Program
     {
-        static void Main(string[] args)
+        protected static string ServerAddress => "localhost";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Sending properties
+            var topic = "andy840119/iot";
+            var quality = MqttQualityOfServiceLevel.AtLeastOnce;
+
+            //Run a MQTT Server
+            var server = new MqttFactory().CreateMqttServer();
+            await server.StartAsync(new MqttServerOptions());
+
+            //Run a MQTT receive client
+            var receniveClient = new MqttFactory().CreateMqttClient();
+            await receniveClient.ConnectAsync(new MqttClientOptionsBuilder().WithTcpServer(ServerAddress).Build());
+            receniveClient.ApplicationMessageReceived += (object o, MqttApplicationMessageReceivedEventArgs e) =>
+            { 
+                //Record received message.
+                var receiveBytes = e.ApplicationMessage.Payload;
+                var receiveMessage = Encoding.UTF8.GetString(receiveBytes);
+                
+                Console.WriteLine(receiveMessage);
+            };
+
+            //Receive client subscribe a topic
+            await receniveClient.SubscribeAsync(new TopicFilterBuilder().WithTopic(topic).WithQualityOfServiceLevel(quality).Build());
+
+            //Prevent server stop
+            Console.WriteLine("Enter to stop");
+            Console.ReadLine();
         }
     }
 }
